ea75224a15c9cce0b60e31ff4c9e71d4409d828f
Tanmay Chaudhary <tanmaychaudhary911@gmail.com>
2025-09-19 21:13:23 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -5,9 +5,12 @@ const pool = new Pool({
   host: 'ep-bold-smoke-ad0pqel1-pooler.c-2.us-east-1.aws.neon.tech', // Database host
   database: 'drive', // Name of the database
   password: 'npg_BZ0g9lcpiToj', // Database user password
-  port: 5432, // Default PostgreSQL port
-    sslmode: 'require' // Require SSL connection
+  port: 5432, // Default PostgreSQL port  
+  ssl: {
+    rejectUnauthorized: false // this is important to bypass unauthorized SSL certificates (useful in some cases)
+  }
 });
 
 module.exports = pool;
+
 
