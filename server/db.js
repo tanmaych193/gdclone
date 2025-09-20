41f44d189d2d175bf3431c8e8d7926b6e2649a00
Tanmay Chaudhary <tanmaychaudhary911@gmail.com>
2025-09-20 12:20:05 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
   user: 'neondb_owner', // PostgreSQL user
   host: 'ep-bold-smoke-ad0pqel1-pooler.c-2.us-east-1.aws.neon.tech', // Database host
@@ -10,8 +9,4 @@ const pool = new Pool({
     rejectUnauthorized: false // this is important to bypass unauthorized SSL certificates (useful in some cases)
   }
 });
-
 module.exports = pool;
-
-
-
